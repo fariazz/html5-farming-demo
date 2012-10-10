8decc5ad4c26dc45bce90d61923125a168958135
Pablo Farias Navarro <fariazz@gmail.com>
2012-10-10 14:35:40 -0300

    fixed farming land states definition

--- land.js
+++ land.js
@@ -1,12 +1,6 @@
 goog.provide('farming.Land');
 goog.require('lime.Sprite');
 
-//states
-farming.EMPTY = 0;
-farming.PLOWED = 1;
-farming.GROWING = 2;
-farming.READY = 3;
-
 /**
  * Land elements
  * 
@@ -18,25 +12,25 @@ farming.Land = function(gameObj, playerObj) {
     this.setSize(gameObj.tile_size,gameObj.tile_size);
     this.setFill('images/bare_land.png');
     
-    this.state = farming.EMPTY;
+    this.state = this.EMPTY;
     
     //user input
     var land = this;
     goog.events.listen(this,['mousedown', 'touchstart'], function(e) {
         e.event.stopPropagation();        
-        if(land.state == farming.EMPTY && playerObj.money >= gameObj.costPlowing) {
+        if(land.state == land.EMPTY && playerObj.money >= gameObj.costPlowing) {
             //plow land
             land.setFill('images/plowed.png')
-            land.state = farming.PLOWED;
+            land.state = land.PLOWED;
             
             //update player money
             playerObj.money -= gameObj.costPlowing;
             gameObj.updateMoney();
         }
-        else if(land.state == farming.PLOWED && playerObj.money >= gameObj.crops[playerObj.currentCrop].cost) {
+        else if(land.state == land.PLOWED && playerObj.money >= gameObj.crops[playerObj.currentCrop].cost) {
             //plant
             land.setFill('images/growing.png');
-            land.state = farming.GROWING;
+            land.state = land.GROWING;
             
             //store crop and left time for it to be ready and to die
             land.crop = playerObj.currentCrop;
@@ -47,10 +41,10 @@ farming.Land = function(gameObj, playerObj) {
             playerObj.money -= gameObj.crops[playerObj.currentCrop].cost;
             gameObj.updateMoney();
         }
-        else if(land.state == farming.READY ) {
+        else if(land.state == land.READY ) {
             //harvest
             land.setFill('images/bare_land.png');
-            land.state = farming.EMPTY;
+            land.state = land.EMPTY;
             
             //update player money
             playerObj.money += gameObj.crops[land.crop].revenue;
@@ -61,18 +55,18 @@ farming.Land = function(gameObj, playerObj) {
     //growing plants
     dt = 1000;
     lime.scheduleManager.scheduleWithDelay(function() {
-        if(this.state == farming.GROWING) {            
+        if(this.state == this.GROWING) {            
             if(this.ripeTime <= 0) {
-                this.state = farming.READY;
+                this.state = this.READY;
                 this.setFill('images/'+gameObj.crops[this.crop].image);
             }
             else {
                 this.ripeTime -= dt;
             }
         }
-        else if(this.state == farming.READY) {
+        else if(this.state == this.READY) {
             if(this.deathTime <= 0) {
-                this.state = farming.EMPTY;
+                this.state = this.EMPTY;
                 this.setFill('images/bare_land.png');
             }
             else {
@@ -83,3 +77,9 @@ farming.Land = function(gameObj, playerObj) {
 }
 
 goog.inherits(farming.Land,lime.Sprite);
+
+//states
+farming.Land.prototype.EMPTY = 0;
+farming.Land.prototype.PLOWED = 1;
+farming.Land.prototype.GROWING = 2;
+farming.Land.prototype.READY = 3;
